a4ca6a8d942b0ab0e2a7c3d6c256ef71f0e49be3
DISN-kolo <arrtemsky@gmail.com>
2024-10-05 16:09:36 +0200

    zoom functionality

--- godot/main_visualizer/camera_rotator.gd
+++ godot/main_visualizer/camera_rotator.gd
@@ -2,8 +2,14 @@ extends Node3D
 
 @onready var camera = $Camera3D
 
-const SENSITIVITY = 0.002
+const SENSITIVITY_DEFAULT = 0.0025
+var sensitivity : float = SENSITIVITY_DEFAULT
 var clickHeld : bool = false
+
+var desiredFov : float = 90
+const FOV_MIN = 10.0
+const FOV_MAX = 90.0
+const ZOOM_FACTOR = 1.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,8 +18,8 @@ func _ready() -> void:
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and clickHeld:
-		self.rotate_y(event.relative.x * SENSITIVITY)
-		camera.rotate_x(event.relative.y * SENSITIVITY)
+		self.rotate_y(event.relative.x * sensitivity)
+		camera.rotate_x(event.relative.y * sensitivity)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,4 +28,11 @@ func _process(delta: float) -> void:
 		clickHeld = true
 	if Input.is_action_just_released("LMB"):
 		clickHeld = false
-	pass
+		
+	if Input.is_action_just_pressed("zoom_in"):
+		desiredFov /= ZOOM_FACTOR
+	elif Input.is_action_just_pressed("zoom_out"):
+		desiredFov *= ZOOM_FACTOR
+	desiredFov = clamp(desiredFov, FOV_MIN, FOV_MAX)
+	sensitivity = desiredFov / FOV_MAX * SENSITIVITY_DEFAULT
+	camera.set_fov(desiredFov)
